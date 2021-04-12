3f137f1ff2e0b7628b5fadd9b669ca1a02a955ce
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-04-12 17:54:29 +0530

    Split Up files

---
+++ src/main/java/TypewiseAlert/CoolingType.java
@@ -0,0 +1,25 @@
+package main.java.TypewiseAlert;
+
+public enum CoolingType 
+{
+    PASSIVE_COOLING(0,35),
+    HI_ACTIVE_COOLING(0,45),
+    MED_ACTIVE_COOLING(0,40);
+    
+    int lowerLimit;
+	int upperLimit;
+	
+	private CoolingType(int lowerLimit,int upperLimit) {
+		this.lowerLimit = lowerLimit;
+		this.upperLimit = upperLimit;
+	}
+
+	public int getLowerLimit() {
+		return lowerLimit;
+	}
+
+	public int getUpperLimit() {
+		return upperLimit;
+	}
+	
+  }
