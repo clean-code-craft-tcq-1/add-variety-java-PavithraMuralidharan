7921e03ee1a104a815cde337d505b8468cef9a2e
PavithraMuralidharan <77607394+PavithraMuralidharan@users.noreply.github.com>
2021-04-22 19:37:32 +0530

    Delete IServiceLocator.java

--- src/main/java/TypewiseAlert/ServiceLocator/IServiceLocator.java
+++
@@ -1,6 +0,0 @@
-package ServiceLocator;
-
-
-public interface IServiceLocator {
-	public <T> T getAleterService(String targetName) throws InstantiationException, IllegalAccessException, ClassNotFoundException;
-}
